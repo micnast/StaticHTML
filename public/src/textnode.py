c4998b59acfc825fd5f2521d82b44bbef8b37956
micnast <micnast@gmail.com>
2024-03-13 15:47:42 +1100

    finished text to textnodes

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -15,12 +15,14 @@ class TextNode:
     def __repr__ (self):
         return f"TextNode({self.text}, {self.text_type}, {self.url}, {self.alt})"
 
+
 text_type_bold = 'bold'       
 text_type_italic = 'italic'
 text_type_code = 'code'
 text_type_text = 'text'
 text_type_link = 'link'
 text_type_image = 'image'
+
 delimiter_keys = {'**':text_type_bold,
                         '*':text_type_italic,
                         '`':text_type_code, 
@@ -38,19 +40,21 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if not isinstance(node, TextNode):
             new_nodes.append(node)
         if isinstance(node, TextNode):
-            counter = 0
-            split_text_list = node.text.split(delimiter)
-            for section in split_text_list:
-                counter +=1
-                if section =='':
-                    continue
-                if counter % 2 != 0:
-                    new_nodes.append(TextNode(section, text_type_text))
-                else:
-                   new_nodes.append(TextNode(section, delimiter_keys[delimiter]))
+            if node.text_type != text_type_text:
+                new_nodes.append(node)
+                continue
+            else:
+                counter = 0
+                split_text_list = node.text.split(delimiter)
+                for section in split_text_list:
+                    counter +=1
+                    if section =='':
+                        continue
+                    if counter % 2 != 0:
+                        new_nodes.append(TextNode(section, text_type_text))
+                    else:
+                       new_nodes.append(TextNode(section, text_type))
     return new_nodes
-
-
 
 def extract_markdown_images(text):
     matches = re.findall(r"!\[(.*?)\]\((.*?)\)",text)
@@ -59,14 +63,6 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"\[(.*?)\]\((.*?)\)",text)
     return matches
-
-node_image = TextNode(
-    "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png) and now with more text again.",
-    text_type_text,
-)
-
-node_link = TextNode("This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)", text_type_text)
-
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -79,14 +75,16 @@ def split_nodes_image(old_nodes):
                 image_tup = extract_markdown_images(leftover[0])
                 if image_tup == []:
                     if leftover == ['']:
-                        return new_nodes
+                        leftover == []
+                        continue
                     else:
-                        new_nodes.append(TextNode(leftover, text_type_text, node.url, node.alt))
-                        return new_nodes
+                        new_nodes.append(TextNode(leftover[0], node.text_type, node.url, node.alt))
+                        leftover=[]
+                        continue
                 else:
                     split_text_list = leftover[0].split(f"![{image_tup[0][0]}]({image_tup[0][1]})",1)
-                    new_nodes.append(TextNode(f'"{split_text_list[0]}"', text_type_text))
-                    new_nodes.append(TextNode(f'"{image_tup[0][0]}"', text_type_image, f'"{image_tup[0][1]}"'))
+                    new_nodes.append(TextNode(split_text_list[0], node.text_type))
+                    new_nodes.append(TextNode(image_tup[0][0], "text_type_image", f'"{image_tup[0][1]}"'))
                     leftover=[]
                     leftover.append(split_text_list[1])
     return new_nodes
@@ -102,24 +100,42 @@ def split_nodes_link(old_nodes):
                 link_tup = extract_markdown_links(leftover[0])
                 if link_tup == []:
                     if leftover == ['']:
-                        return new_nodes
+                        leftover = []
+                        continue
                     else:
-                        new_nodes.append(TextNode(leftover, text_type_text, node.url, node.alt))
-                        return new_nodes
+                        new_nodes.append(TextNode(leftover[0], node.text_type, node.url, node.alt))
+                        leftover = []
+                        continue
                 else:
                     split_text_list = leftover[0].split(f"[{link_tup[0][0]}]({link_tup[0][1]})",1)
-                    new_nodes.append(TextNode(f'"{split_text_list[0]}"', text_type_text))
-                    new_nodes.append(TextNode(f'"{link_tup[0][0]}"', text_type_link, f'"{link_tup[0][1]}"'))
+                    new_nodes.append(TextNode(split_text_list[0], node.text_type))
+                    new_nodes.append(TextNode(link_tup[0][0], "text_type_link", f'"{link_tup[0][1]}"'))
                     leftover = []
                     leftover.append(split_text_list[1])
     return new_nodes
 
+def text_to_textnodes(node_list):
+    split_link_nodes = []
+    split_delimeter_nodes = []
+    delimiter_bold = split_nodes_delimiter(node_list,'**', text_type_bold)
+    delimiter_italic = split_nodes_delimiter(delimiter_bold,'*', text_type_italic)
+    delimiter_code = split_nodes_delimiter(delimiter_italic,'`', text_type_code)
+    split_image = split_nodes_image(delimiter_code)
+    split_link = split_nodes_link(split_image)
+    return split_link
+    
+    
+    
 
 
-test_image = split_nodes_image([node_image])
-test_link = split_nodes_link([node_link])
-
-print(test_image)
-print(test_link)
+# Do the delimiter last cos it wants to go through a list of nodes anyway. 
 
 
+test_text = "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+node_complete = TextNode(
+    "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)",
+    text_type_text, None, None)
+node_list = [node_complete]
+
+
+print(text_to_textnodes(node_list))
